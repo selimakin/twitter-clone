577bfa932f797b3d3de34edb738a806edcc19fc9
Selim <selimakin55@gmail.com>
2022-11-09 21:28:47 +0300

    Register component and signup page are implemented

---
+++ pages/signup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "../components/entrance/Register";
+
+const signup = () => {
+  return (
+    <div className="relative h-screen bg-neutral-400">
+      <Register />
+    </div>
+  );
+};
+
+export default signup;
