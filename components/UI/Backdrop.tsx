d1354d72d16e70c3d90b9315a088cf59f093b355
Selim <selimakin55@gmail.com>
2022-11-12 16:25:04 +0300

    Backdrop components is implemented

---
+++ components/UI/Backdrop.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Backdrop = () => {
+  return (
+    <div className="fixed top-0 left-0 w-screen h-screen bg-gray-300"></div>
+  );
+};
+
+export default Backdrop;
