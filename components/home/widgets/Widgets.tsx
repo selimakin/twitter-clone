42fc405226403bf19b9f13531623823d95f3a3ce
Selim <selimakin55@gmail.com>
2022-11-13 23:03:52 +0300

    Twitter icon is implemented

--- components/home/widgets/Widgets.tsx
+++ components/home/widgets/Widgets.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import SearchIcon from "@mui/icons-material/Search";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 const Widgets = () => {
   return (
-    <div className="col-span-2 mt-2 px-2 hidden lg:inline">
-      <div className="flex items-center space-x-2 bg-gray-100 p-3 my-2 rounded-full">
-        <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+    <div className="hidden col-span-2 px-2 mt-2 lg:inline">
+      <div className="flex items-center p-3 my-2 space-x-2 bg-gray-100 rounded-full">
+        <SearchIcon className="text-gray-400 " />
         <input
           type="text"
           placeholder="Search Twitter"
-          className="bg-transparent flex-1 outline-none"
+          className="flex-1 bg-transparent outline-none"
         />
       </div>
       <TwitterTimelineEmbed
